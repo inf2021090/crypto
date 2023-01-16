70daec2c5fc1eac28acdbe1536f14ad280d1e882
inf2021090 <inf2021090@ionio.gr>
2023-01-16 18:43:58 +0200

    done

--- aes.sh
+++ aes.sh
@@ -8,13 +8,13 @@ IV=00010203040506070809aabbccddeeff
 	echo "...encryption..."
 	read -p "enter file name: " FN
 	read -p "enter output: " O
-	openssl enc -aes-256-cbc -e -in $FN -out $O -K $K -iv $IV
+	openssl enc -aes-128-cbc -e -in $FN -out $O -K $K -iv $IV
 
 ###########DECRYPTION########################	
 	echo "...decryption..."
 	read -p "enter file name: " FN
 	read -p "enter output: " O
-	openssl enc -d -aes-256-cbc -e -in $FN -out -K $K -iv $IV
+	openssl enc -aes-128-cbc -e -in $FN -out $O -d  -K $K -iv $IV
 
 
 
